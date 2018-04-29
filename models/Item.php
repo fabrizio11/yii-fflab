34e2380d96683846809b802f002d12ee696114bd
Fabrizio Ferrigno <fabo92@gmail.com>
2018-04-29 13:02:52 +0200

    Add Item model

---
+++ models/Item.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "item".
+ *
+ * @property int $item_id
+ * @property string $name
+ * @property string $description
+ * @property int $category_id
+ *
+ * @property Category $category
+ */
+class Item extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['description'], 'string'],
+            [['category_id'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::className(), 'targetAttribute' => ['category_id' => 'category_id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'item_id' => 'Item ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'category_id' => 'Category ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::className(), ['category_id' => 'category_id']);
+    }
+}
