7bd6f7a122cd59d29b75177d1d62886bb3145d48
Fabrizio Ferrigno <fabo92@gmail.com>
2018-04-29 13:02:45 +0200

    Add Category model

---
+++ models/Category.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $category_id
+ * @property string $name
+ * @property string $description
+ *
+ * @property Item[] $items
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['description'], 'string'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'category_id' => 'Category ID',
+            'name' => 'Name',
+            'description' => 'Description',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['category_id' => 'category_id']);
+    }
+}
